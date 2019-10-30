cd20d3525af9795a3e304d535b61618ace2953af
mino2357 <mino2357@gmail.com>
2019-10-30 12:25:06 +0900

    RK以外は動く．残りを修正中．

--- app/main.cpp
+++ app/main.cpp
@@ -1,17 +1,27 @@
+/*
+ * 移流のテスト
+ * 
+ * 移流方程式/バーガース方程式
+ */
+
 #include <iostream>
 #include <array>
 #include <cmath>
 #include <limits>
 #include <iomanip>
 
-const double Lx = 1.0;
-const double Ly = 1.0;
-const int Nx    = 64;
-const int Ny    = 64;
-const double dx = Lx / (Nx - 1);
-const double dy = Ly / (Ny - 1);
-const double dt = 0.0001;
-const int INTV  = 100;
+// parameters
+const double Lx    = 1.0;
+const double Ly    = 1.0;
+const int Nx       = 64;
+const int Ny       = 64;
+const double dx    = Lx / (Nx - 1);
+const double dy    = Ly / (Ny - 1);
+const double a     = 1.0; // advection [a, b]^T
+const double b     = 1.0; // vector field
+const double alpha = 1.0;   // 1:UTOPIA 3:K-K
+const double dt    = 0.01 / (std::sqrt(a * a + b * b) * std::sqrt((1.0 / dx) * (1.0 / dx) + (1.0 / dy) * (1.0 / dy)));
+const int INTV     = 100;
 
 namespace rittai3d{
 	namespace utility{
@@ -42,15 +52,19 @@ namespace sksat {
 
 using Array = sksat::array_wrapper<Nx, sksat::array_wrapper<Ny>>;;
 
+/********************************/
+/*      up wind scheme          */
+/********************************/
+
 void upWind_firstOrder_X(Array& u, Array& v, Array& u_next){
     double ad1, ad2;
     for(int i=0; i<Nx; ++i){
         for(int j=0; j<Ny; ++j){
-            ad1 = 1.0; // u[i][j];
-            ad2 = 1.0; // v[i][j];
-            u_next[i][j] = u[i][j]
-                            - dt * ( ad1 * (u[i+1][j] - u[i-1][j]) / (2.0 * dx) + std::abs(ad1) * (- u[i+1][j] + 2.0 * u[i][j] - u[i-1][j]) / (2.0 * dx))
-                            - dt * ( ad2 * (u[i][j+1] - u[i][j-1]) / (2.0 * dy) + std::abs(ad2) * (- u[i][j+1] + 2.0 * u[i][j] - u[i][j-1]) / (2.0 * dy));
+            ad1 = a; // u[i][j];
+            ad2 = b; // v[i][j];
+            u_next[i][j] =
+                - ( ad1 * (u[i+1][j] - u[i-1][j]) / (2.0 * dx) + std::abs(ad1) * (- u[i+1][j] + 2.0 * u[i][j] - u[i-1][j]) / (2.0 * dx))
+                - ( ad2 * (u[i][j+1] - u[i][j-1]) / (2.0 * dy) + std::abs(ad2) * (- u[i][j+1] + 2.0 * u[i][j] - u[i][j-1]) / (2.0 * dy));
         }
     }
 }
@@ -59,15 +73,84 @@ void upWind_firstOrder_Y(Array& u, Array& v, Array& v_next){
     double ad1, ad2;
     for(int i=0; i<Nx; ++i){
         for(int j=0; j<Ny; ++j){
-            ad1 = 1.0; // u[i][j];
-            ad2 = 1.0; // v[i][j];
-            v_next[i][j] = v[i][j]
-                            - dt * ( ad1 * (v[i+1][j] - v[i-1][j]) / (2.0 * dx) + std::abs(ad1) * (- v[i+1][j] + 2.0 * v[i][j] - v[i-1][j]) / (2.0 * dx))
-                            - dt * ( ad2 * (v[i][j+1] - v[i][j-1]) / (2.0 * dy) + std::abs(ad2) * (- v[i][j+1] + 2.0 * v[i][j] - v[i][j-1]) / (2.0 * dy));
+            ad1 = a; // u[i][j];
+            ad2 = b; // v[i][j];
+            v_next[i][j] =
+                - ( ad1 * (v[i+1][j] - v[i-1][j]) / (2.0 * dx) + std::abs(ad1) * (- v[i+1][j] + 2.0 * v[i][j] - v[i-1][j]) / (2.0 * dx))
+                - ( ad2 * (v[i][j+1] - v[i][j-1]) / (2.0 * dy) + std::abs(ad2) * (- v[i][j+1] + 2.0 * v[i][j] - v[i][j-1]) / (2.0 * dy));
         }
     }
 }
 
+/********************************/
+/*      K-K/UTOPIA scheme       */
+/********************************/
+
+void upWind_thirdOrder_X(Array& u, Array& v, Array& u_next){
+    double ad1, ad2;
+    for(int i=0; i<Nx; ++i){
+        for(int j=0; j<Ny; ++j){
+            ad1 = a; // u[i][j];
+            ad2 = b; // v[i][j];
+            u_next[i][j] =
+                - ( ad1 * ( - u[i+2][j] + 8.0 * (u[i+1][j] - u[i-1][j]) + u[i-2][j]) / (12.0 * dx)
+                    + alpha * std::abs(ad1) * ( u[i+2][j] - 4.0 * u[i+1][j] + 6.0 * u[i][j] - 4.0 * u[i-1][j] + u[i-2][j]) / (12.0 * dx))
+                - ( ad2 * ( - u[i][j+2] + 8.0 * (u[i][j+1] - u[i][j-1]) + u[i][j-2]) / (12.0 * dy)
+                    + alpha * std::abs(ad2) * ( u[i][j+2] - 4.0 * u[i][j+1] + 6.0 * u[i][j] - 4.0 * u[i][j-1] + u[i][j-2]) / (12.0 * dy));
+        }
+    }
+}
+
+void upWind_thirdOrder_Y(Array& u, Array& v, Array& v_next){
+    double ad1, ad2;
+    for(int i=0; i<Nx; ++i){
+        for(int j=0; j<Ny; ++j){
+            ad1 = a; // u[i][j];
+            ad2 = b; // v[i][j];
+            v_next[i][j] =
+                - ( ad1 * (- v[i+2][j] + 8.0 * (v[i+1][j] - v[i-1][j]) + v[i-2][j] ) / (12.0 * dx)
+                    + alpha * std::abs(ad1) * ( v[i+2][j] - 4.0 * v[i+1][j] + 6.0 * v[i][j] - 4.0 * v[i-1][j] + v[i-2][j]) / (12.0 * dx))
+                - ( ad2 * ( - v[i][j+2] + 8.0 * (v[i][j+1] - v[i][j-1]) + v[i][j-2]) / (12.0 * dy)
+                    + alpha * std::abs(ad2) * ( v[i][j+2] - 4.0 * v[i][j+1] + 6.0 * v[i][j] - 4.0 * v[i][j-1] + v[i][j-2]) / (12.0 * dy));
+        }
+    }
+}
+
+/********************************/
+/*      up wind 5th scheme      */
+/********************************/
+
+void upWind_fifthOrder_X(Array& u, Array& v, Array& u_next){
+    double ad1, ad2;
+    for(int i=0; i<Nx; ++i){
+        for(int j=0; j<Ny; ++j){
+            ad1 = a; // u[i][j];
+            ad2 = b; // v[i][j];
+            u_next[i][j] =
+                - ( ad1 * ( u[i+3][j] - 9.0 * u[i+2][j] + 45.0 * (u[i+1][j] - u[i-1][j]) + 9.0 * u[i-2][j] - u[i-3][j] ) / (60.0 * dx)
+                    + std::abs(ad1) * ( - u[i+3][j] + 6.0 * u[i+2][j] - 15.0 * u[i+1][j] + 20.0 * u[i][j] - 15.0 * u[i-1][j] + 6.0 * u[i-2][j] - u[i-3][j] ) / (60.0 * dx))
+                - ( ad2 * ( u[i][j+3] - 9.0 * u[i][j+2] + 45.0 * (u[i][j+1] - u[i][j-1]) + 9.0 * u[i][j-2] - u[i][j-3] ) / (60.0 * dy)
+                    + std::abs(ad2) * ( - u[i][j+3] + 6.0 * u[i][j+2] - 15.0 * u[i][j+1] + 20.0 * u[i][j] - 15.0 * u[i][j-1] + 6.0 * u[i][j-2] - u[i][j-3] ) / (60.0 * dy));
+        }
+    }
+}
+
+void upWind_fifthOrder_Y(Array& u, Array& v, Array& v_next){
+    double ad1, ad2;
+    for(int i=0; i<Nx; ++i){
+        for(int j=0; j<Ny; ++j){
+            ad1 = a; // u[i][j];
+            ad2 = b; // v[i][j];
+            v_next[i][j] =
+                - ( ad1 * ( v[i+3][j] - 9.0 * v[i+2][j] + 45.0 * (v[i+1][j] - v[i-1][j]) + 9.0 * v[i-2][j] - v[i-3][j] ) / (60.0 * dx)
+                    + std::abs(ad1) * ( - v[i+3][j] + 6.0 * v[i+2][j] - 15.0 * v[i+1][j] + 20.0 * v[i][j] - 15.0 * v[i-1][j] + 6.0 * v[i-2][j] - v[i-3][j] ) / (60.0 * dx))
+                - ( ad2 * ( v[i][j+3] - 9.0 * v[i][j+2] + 45.0 * (v[i][j+1] - v[i][j-1]) + 9.0 * v[i][j-2] - v[i][j-3] ) / (60.0 * dy)
+                    + std::abs(ad2) * ( - v[i][j+3] + 6.0 * v[i][j+2] - 15.0 * v[i][j+1] + 20.0 * v[i][j] - 15.0 * v[i][j-1] + 6.0 * v[i][j-2] - v[i][j-3] ) / (60.0 * dy));
+        }
+    }
+}
+
+// clear
 void clear(Array& u, Array& v, Array& u_next, Array& v_next){
     for(int i=0; i<Nx; ++i){
         for(int j=0; j<Ny; ++j){
@@ -77,6 +160,7 @@ void clear(Array& u, Array& v, Array& u_next, Array& v_next){
     }
 }
 
+// initialize function
 void init_func(Array& u, Array& v){
     for(int i=0; i<Nx; ++i){
         for(int j=0; j<Ny; ++j){
@@ -88,6 +172,249 @@ void init_func(Array& u, Array& v){
     }
 }
 
+// Schemes
+void Euler_1stOrder(Array& u, Array& v, Array& u_next, Array& v_next){
+    Array du = {};
+    Array dv = {};
+    upWind_firstOrder_X(u, v, du);
+    upWind_firstOrder_Y(u, v, dv);
+    for(int i=0; i<Nx; ++i){
+        for(int j=0; j<Ny; ++j){
+            u_next[i][j] = u[i][j] + dt * du[i][j];
+            v_next[i][j] = v[i][j] + dt * dv[i][j];
+        }
+    }
+}
+
+void Euler_3rdOrder(Array& u, Array& v, Array& u_next, Array& v_next){
+    Array du = {};
+    Array dv = {};
+    upWind_thirdOrder_X(u, v, du);
+    upWind_thirdOrder_Y(u, v, dv);
+    for(int i=0; i<Nx; ++i){
+        for(int j=0; j<Ny; ++j){
+            u_next[i][j] = u[i][j] + dt * du[i][j];
+            v_next[i][j] = v[i][j] + dt * dv[i][j];
+        }
+    }
+}
+
+void TVD_RK3_3rdOrder(Array& u, Array& v, Array& u_next, Array& v_next){
+    Array du1 = {};
+    Array dv1 = {};
+    Array u1  = {};
+    Array v1  = {};
+    upWind_thirdOrder_X(u ,v, du1);
+    upWind_thirdOrder_Y(u ,v, dv1);
+    for(int i=0; i<Nx; ++i){
+        for(int j=0; j<Ny; ++j){
+            u1[i][j] = u[i][j] + dt * du1[i][j];
+            v1[i][j] = v[i][j] + dt * dv1[i][j];
+        }
+    }
+    Array du2 = {};
+    Array dv2 = {};
+    Array u2  = {};
+    Array v2  = {};
+    upWind_thirdOrder_X(u1 ,v1, du2);
+    upWind_thirdOrder_Y(u1 ,v1, dv2);
+    for(int i=0; i<Nx; ++i){
+        for(int j=0; j<Ny; ++j){
+            u2[i][j] = 3.0 / 4.0 * u[i][j] + 1.0 / 4.0 * u1[i][j] + 1.0 / 4.0 * dt * du2[i][j];
+            v2[i][j] = 3.0 / 4.0 * v[i][j] + 1.0 / 4.0 * v1[i][j] + 1.0 / 4.0 * dt * dv2[i][j];
+        }
+    }
+    Array du3 = {};
+    Array dv3 = {};
+    upWind_thirdOrder_X(u2 ,v2, du3);
+    upWind_thirdOrder_Y(u2 ,v2, dv3);
+    for(int i=0; i<Nx; ++i){
+        for(int j=0; j<Ny; ++j){
+            u_next[i][j] = 1.0 / 3.0 * u[i][j] + 2.0 / 3.0 * u2[i][j] + 2.0 / 3.0 * dt * du3[i][j];
+            v_next[i][j] = 1.0 / 3.0 * v[i][j] + 2.0 / 3.0 * v2[i][j] + 2.0 / 3.0 * dt * dv3[i][j];
+        }
+    }
+}
+
+Array operator+(Array& u, Array& v){
+    Array ans = {};
+    for(int i=0; i<Nx; ++i){
+        for(int j=0; j<Ny; ++j){
+            ans[i][j] = u[i][j] + v[i][j];
+        }
+    }
+    return ans;
+}
+
+Array operator*(double a, Array& u){
+    Array ans = {};
+    for(int i=0; i<Nx; ++i){
+        for(int j=0; j<Ny; ++j){
+            ans[i][j] = a * u[i][j];
+        }
+    }
+    return ans;
+}
+
+void RK3_3rdOrder(Array& u, Array& v, Array& u_next, Array& v_next){
+    Array du1 = {};
+    Array dv1 = {};
+    Array u1  = {};
+    Array v1  = {};
+    upWind_thirdOrder_X(u ,v, du1);
+    upWind_thirdOrder_Y(u ,v, dv1);
+    for(int i=0; i<Nx; ++i){
+        for(int j=0; j<Ny; ++j){
+            u1[i][j] = dt * du1[i][j];
+            v1[i][j] = dt * dv1[i][j];
+        }
+    }
+    Array du2 = {};
+    Array dv2 = {};
+    Array u2  = {};
+    Array v2  = {};
+    upWind_thirdOrder_X(u1 , v1, du2);
+    upWind_thirdOrder_Y(u1 , v1, dv2);
+    for(int i=0; i<Nx; ++i){
+        for(int j=0; j<Ny; ++j){
+            u2[i][j] = 1.0 / 2.0 * dt * du2[i][j];
+            v2[i][j] = 1.0 / 2.0 * dt * dv2[i][j];
+        }
+    }
+    Array du3 = {};
+    Array dv3 = {};
+    Array u3  = {};
+    Array v3  = {};
+    upWind_thirdOrder_X(u2 ,v2, du3);
+    upWind_thirdOrder_Y(u2 ,v2, dv3);
+    for(int i=0; i<Nx; ++i){
+        for(int j=0; j<Ny; ++j){
+            u3[i][j] = - 1.0 * dt * du2[i][j] + 2.0 * dt * du3[i][j];
+            v3[i][j] = - 1.0 * dt * dv2[i][j] + 2.0 * dt * dv3[i][j];
+        }
+    }
+    for(int i=0; i<Nx; ++i){
+        for(int j=0; j<Ny; ++j){
+            u_next[i][j] = u[i][j] + 1.0 / 6.0 * u1[i][j] + 2.0 / 3.0 * u2[i][j] + 1.0 / 6.0 * u3[i][j];
+            v_next[i][j] = v[i][j] + 1.0 / 6.0 * v1[i][j] + 2.0 / 3.0 * v2[i][j] + 1.0 / 6.0 * v3[i][j];
+        }
+    }
+}
+
+void RK4_3rdOrder(Array& u, Array& v, Array& u_next, Array& v_next){
+    Array du1 = {};
+    Array dv1 = {};
+    Array u1  = {};
+    Array v1  = {};
+    upWind_thirdOrder_X(u ,v, du1);
+    upWind_thirdOrder_Y(u ,v, dv1);
+    for(int i=0; i<Nx; ++i){
+        for(int j=0; j<Ny; ++j){
+            u1[i][j] = u[i][j] + dt * du1[i][j];
+            v1[i][j] = v[i][j] + dt * dv1[i][j];
+        }
+    }
+    Array du2 = {};
+    Array dv2 = {};
+    Array u2  = {};
+    Array v2  = {};
+    upWind_thirdOrder_X(u1 ,v1, du2);
+    upWind_thirdOrder_Y(u1 ,v1, dv2);
+    for(int i=0; i<Nx; ++i){
+        for(int j=0; j<Ny; ++j){
+            u2[i][j] = u[i][j] + 1.0 / 2.0 * dt * du2[i][j];
+            v2[i][j] = v[i][j] + 1.0 / 2.0 * dt * dv2[i][j];
+        }
+    }
+    Array du3 = {};
+    Array dv3 = {};
+    Array u3  = {};
+    Array v3  = {};
+    upWind_thirdOrder_X(u2 ,v2, du3);
+    upWind_thirdOrder_Y(u2 ,v2, dv3);
+    for(int i=0; i<Nx; ++i){
+        for(int j=0; j<Ny; ++j){
+            u3[i][j] = u[i][j] + 1.0 / 2.0 * dt * du3[i][j];
+            v3[i][j] = v[i][j] + 1.0 / 2.0 * dt * dv3[i][j];
+        }
+    }
+    Array du4 = {};
+    Array dv4 = {};
+    Array u4  = {};
+    Array v4  = {};
+    upWind_thirdOrder_X(u3 ,v3, du4);
+    upWind_thirdOrder_Y(u3 ,v3, dv4);
+    for(int i=0; i<Nx; ++i){
+        for(int j=0; j<Ny; ++j){
+            u4[i][j] = u[i][j] + 1.0 / 2.0 * dt * du3[i][j];
+            v4[i][j] = v[i][j] + 1.0 / 2.0 * dt * dv3[i][j];
+        }
+    }
+    for(int i=0; i<Nx; ++i){
+        for(int j=0; j<Ny; ++j){
+            u_next[i][j] = 1.0 / 6.0 * u1[i][j] + 1.0 / 3.0 * u2[i][j] + 1.0 / 3.0 * u3[i][j] + 1.0 / 6.0 * u4[i][j];
+            v_next[i][j] = 1.0 / 6.0 * v1[i][j] + 1.0 / 3.0 * v2[i][j] + 1.0 / 3.0 * v3[i][j] + 1.0 / 6.0 * v4[i][j];
+        }
+    }
+}
+
+void RK4_5rdOrder(Array& u, Array& v, Array& u_next, Array& v_next){
+    Array du1 = {};
+    Array dv1 = {};
+    Array u1  = {};
+    Array v1  = {};
+    upWind_fifthOrder_X(u ,v, du1);
+    upWind_fifthOrder_Y(u ,v, dv1);
+    for(int i=0; i<Nx; ++i){
+        for(int j=0; j<Ny; ++j){
+            u1[i][j] = u[i][j] + dt * du1[i][j];
+            v1[i][j] = v[i][j] + dt * dv1[i][j];
+        }
+    }
+    Array du2 = {};
+    Array dv2 = {};
+    Array u2  = {};
+    Array v2  = {};
+    upWind_fifthOrder_X(u1 ,v1, du2);
+    upWind_fifthOrder_Y(u1 ,v1, dv2);
+    for(int i=0; i<Nx; ++i){
+        for(int j=0; j<Ny; ++j){
+            u2[i][j] = u[i][j] + 1.0 / 2.0 * dt * du2[i][j];
+            v2[i][j] = v[i][j] + 1.0 / 2.0 * dt * dv2[i][j];
+        }
+    }
+    Array du3 = {};
+    Array dv3 = {};
+    Array u3  = {};
+    Array v3  = {};
+    upWind_fifthOrder_X(u2 ,v2, du3);
+    upWind_fifthOrder_Y(u2 ,v2, dv3);
+    for(int i=0; i<Nx; ++i){
+        for(int j=0; j<Ny; ++j){
+            u3[i][j] = u[i][j] + 1.0 / 2.0 * dt * du3[i][j];
+            v3[i][j] = v[i][j] + 1.0 / 2.0 * dt * dv3[i][j];
+        }
+    }
+    Array du4 = {};
+    Array dv4 = {};
+    Array u4  = {};
+    Array v4  = {};
+    upWind_fifthOrder_X(u3 ,v3, du4);
+    upWind_fifthOrder_Y(u3 ,v3, dv4);
+    for(int i=0; i<Nx; ++i){
+        for(int j=0; j<Ny; ++j){
+            u4[i][j] = u[i][j] + 1.0 / 2.0 * dt * du3[i][j];
+            v4[i][j] = v[i][j] + 1.0 / 2.0 * dt * dv3[i][j];
+        }
+    }
+    for(int i=0; i<Nx; ++i){
+        for(int j=0; j<Ny; ++j){
+            u_next[i][j] = 1.0 / 6.0 * u1[i][j] + 1.0 / 3.0 * u2[i][j] + 1.0 / 3.0 * u3[i][j] + 1.0 / 6.0 * u4[i][j];
+            v_next[i][j] = 1.0 / 6.0 * v1[i][j] + 1.0 / 3.0 * v2[i][j] + 1.0 / 3.0 * v3[i][j] + 1.0 / 6.0 * v4[i][j];
+        }
+    }
+}
+
 int main(){
     std::cout << std::fixed << std::setprecision(std::numeric_limits<double>::digits10 - 8);
 
@@ -95,6 +422,7 @@ int main(){
     Array v = {};
     Array u_next = {};
     Array v_next = {};
+    double t = 0.0;
 
     init_func(u, v);
 
@@ -117,7 +445,8 @@ int main(){
     for(int i=0; i<Nx; ++i){
         for(int j=0; j<Ny; ++j){
             norm = std::sqrt(u[i][j] * u[i][j] + v[i][j] * v[i][j]);
-            fprintf(gp, "%f %f %f %f %f\n", i * dx , j * dy, u[i][j] / (norm * Nx), v[i][j] / (norm * Ny), norm);
+            coef = 2.0 * norm;
+            fprintf(gp, "%f %f %f %f %f\n", i * dx , j * dy, coef * u[i][j] / (norm * Nx), coef * v[i][j] / (norm * Ny), norm);
         }
     }
     fprintf(gp, "e\n");
@@ -126,9 +455,13 @@ int main(){
     std::cout << "Enterキーを押してください．" << std::endl;
     getchar();
 
-    for(int i=0;;++i){
-        upWind_firstOrder_X(u ,v, u_next);
-        upWind_firstOrder_Y(u ,v, v_next);
+    for(int i=1; t<1.0; ++i){
+        //Euler_1stOrder(u, v, u_next, v_next);
+        //Euler_3rdOrder(u, v, u_next, v_next);
+        //TVD_RK3_3rdOrder(u, v, u_next, v_next);
+        RK3_3rdOrder(u, v, u_next, v_next);
+        //RK4_3rdOrder(u, v, u_next, v_next);
+        //RK4_5rdOrder(u, v, u_next, v_next);
         clear(u, v, u_next, v_next);
 
         // 描画
@@ -137,11 +470,14 @@ int main(){
             for(int i=0; i<Nx; ++i){
                 for(int j=0; j<Ny; ++j){
                     norm = std::sqrt(u[i][j] * u[i][j] + v[i][j] * v[i][j]);
-                    fprintf(gp, "%f %f %f %f %f\n", i * dx , j * dy, u[i][j] / (norm * Nx), v[i][j] / (norm * Ny), norm);
+                    coef = 2.0 * norm;
+                    fprintf(gp, "%f %f %f %f %f\n", i * dx , j * dy, coef * u[i][j] / (norm * Nx), coef * v[i][j] / (norm * Ny), norm);
                 }
             }
             fprintf(gp, "e\n");
             fflush(gp);
+            std::cout << t << std::endl;
         }
+        t = i * dt;
     }
 }
